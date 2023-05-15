768786d27cd3b7ad02e0b668ce3c12000d1a4b69
Pavel Blokhin <blokhinpavel92@yandex.ru>
2023-05-15 15:09:03 +0300

    add form-address-da-data.html

--- projects/green-controls/src/forms/form-address-da-data/green-form-address-da-data.module.ts
+++ projects/green-controls/src/forms/form-address-da-data/green-form-address-da-data.module.ts
@@ -1,13 +1,23 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { API_ADDRESS_DA_DATA } from 'green-controls/src/token';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { GreenAutoCompliteModule, GreenBasicInputModule } from 'green-controls/src/controls';
 import { FormAddressDaDataComponent } from './form-address-da-data/form-address-da-data.component';
 
 @NgModule({
   declarations: [ FormAddressDaDataComponent ],
-  imports: [ CommonModule, HttpClientModule ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    GreenAutoCompliteModule,
+    GreenBasicInputModule,
+  ],
   exports: [ FormAddressDaDataComponent ],
 })
 export class GreenAddressDaDataModule {
