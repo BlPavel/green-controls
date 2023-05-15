be19f651bc7a645f36d9f664fa19bcf0cf3f01cd
Pavel Blokhin <blokhinpavel92@yandex.ru>
2023-05-15 12:53:04 +0300

    change interface message-error

--- projects/green-controls/src/interfaces/message-error.ts
+++ projects/green-controls/src/interfaces/message-error.ts
@@ -1,12 +1,15 @@
 import { ICustomMessageError } from './custom-message-error';
 
 export interface IMessageError extends ICustomMessageError {
-  'required': string;
-  'email': string;
-  'pattern': string;
-  'notequal': string;
-  'mask': string;
-  'minlength': string;
-  'maxlength': string;
-  'notSelected': string;
+  required: string;
+  email: string;
+  pattern: string;
+  notequal: string;
+  mask: string;
+  minlength: string;
+  maxlength: string;
+  notSelected: string;
+  matDatepickerMax: string,
+  matDatepickerMin: string,
+  matDatepickerParse: string,
 }
